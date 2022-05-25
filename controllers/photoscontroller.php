321ffe5c12222feb4eb877bb35e3abc2a7b49541
TudorPCT <76231144+TudorPCT@users.noreply.github.com>
2022-05-25 09:42:02 +0300

    Signin

---
+++ controllers/photoscontroller.php
@@ -0,0 +1,8 @@
+<?php
+
+class PhotosController extends Controller
+{
+    function __construct(){
+        parent::__construct();
+    }
+}
---
+++ views/templates/scripts/signinscript.js
@@ -0,0 +1,25 @@
+function signin() {
+
+    var formEl = document.forms.signinform;
+    var formData = new FormData(formEl);
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+            myResponse = JSON.parse(this.responseText);
+            alert("Signin successful");
+            localStorage.setItem("jwt", 'Bearer ' + myResponse.jwt);
+            window.location.href = './index.php?load=photos';
+        }else if (this.readyState === XMLHttpRequest.DONE) {
+            myResponse = JSON.parse(this.responseText);
+            alert(myResponse.message);
+            document.getElementById("errorLabel").innerHTML = myResponse.message;
+        }
+
+    };
+    xmlhttp.open("POST","/Tehnologii-Web/index.php?load=Signin/signin", true);
+    xmlhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+    xmlhttp.send("email=" + formData.get('email') +
+        "&password=" + formData.get('password'));
+}
