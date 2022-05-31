f1a8dc3a072cc2fff107d0636f8a7b13ff37b295
Hututuc Felix <felix.hututuc@gmail.com>
2022-05-31 12:17:13 +0300

    remove composer

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
     define ('DS', DIRECTORY_SEPARATOR);
     define ('HOME', dirname(__FILE__));
 
-    require_once HOME . DS . 'vendor' . DS . 'autoload.php';
     require_once HOME . DS . 'config.php';
     require_once HOME . DS . 'utils' . DS . 'autoload.php';
     require_once HOME . DS . 'utils' . DS . 'bootstrap.php';
