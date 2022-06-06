618b05bf84a02b88faf3e2c97d767400c7e797ea
Pasat Tudor <76231144+TudorPCT@users.noreply.github.com>
2022-06-06 18:44:57 +0300

    Update accountscontroller.php

--- controllers/accountscontroller.php
+++ controllers/accountscontroller.php
@@ -16,4 +16,18 @@ class AccountsController extends Controller
         }
 
     }
+    
+        function addUnsplashAccount(){
+  /*      $headers = apache_request_headers();
+        $token = getBearerToken($headers);
+        if ($token === null || !verify_token($token)) {
+            http_response_code(401);
+            exit(401);
+        }else{
+
+        }*/
+
+        header('Location: https://unsplash.com/oauth/authorize?client_id=9XFuoEpXqAkmsha8In-94JPBM9v5nueRYmKKF5uhjTM&redirect_uri=https%3A%2F%2Fsocialmediabox.herokuapp.com&response_type=code&scope=public');
+        die();
+    }
 }
