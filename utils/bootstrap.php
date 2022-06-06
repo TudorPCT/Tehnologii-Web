715d8d7f4b048d1d35a89134f59f6d32ef17b98a
TudorPCT <76231144+TudorPCT@users.noreply.github.com>
2022-06-06 11:38:29 +0300

    .

--- utils/bootstrap.php
+++ utils/bootstrap.php
@@ -1,11 +1,7 @@
 <?php
 
 header("Access-Control-Allow-Origin: null");
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: GET");
-header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-
+header("Access-Control-Allow-Methods: *");
 
 
 if (isset($_GET['code'])) {
