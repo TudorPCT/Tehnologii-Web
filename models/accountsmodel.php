2086477a479afb31f701c620f69c77b9aa3fe8df
DESKTOP-Q4F7MV9\Raluca <raluca1234rr@gmail.com>
2022-06-13 23:14:14 +0300

    .

--- models/accountsmodel.php
+++ models/accountsmodel.php
@@ -8,8 +8,8 @@ class AccountsModel extends Model
     function getAccounts($token){
         $payload=json_decode(extractTokenPayload($token),true);
         $user_id=$payload['id'];
-
-        $this->setSql("select * from accounts;");
+        $querry="select * from accounts where user_id=".$user_id.";";
+        $this->setSql($querry);
         $result = $this->getAll();
         for($index = 0; $index < sizeof($result); $index++) {
             echo $result[$index]["username"] ;
