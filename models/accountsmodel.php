eaddee4469ced984ed77e362c50b409320aaa04a
DESKTOP-Q4F7MV9\Raluca <raluca1234rr@gmail.com>
2022-06-13 23:52:05 +0300

    .

--- models/accountsmodel.php
+++ models/accountsmodel.php
@@ -5,20 +5,5 @@ class AccountsModel extends Model
     function __construct(){
         parent::__construct();
     }
-    function getAccounts($token){
-        $payload=json_decode(extractTokenPayload($token),true);
-        $user_id=$payload['id'];
 
-        $user_id=6;
-
-        $querry="select * from accounts where user_id=".$user_id.";";
-        $this->setSql($querry);
-        $result = $this->getAll();
-        for($index = 0; $index < sizeof($result); $index++) {
-            echo $result[$index]["username"] ;
-            echo "  ";
-        }
-        return true;
-
-    }
 }
