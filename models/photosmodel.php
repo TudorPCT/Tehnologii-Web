960ea0250f3085b546f960446974e0a1d721098b
TudorPCT <76231144+TudorPCT@users.noreply.github.com>
2022-06-13 19:27:29 +0300

    .

--- models/photosmodel.php
+++ models/photosmodel.php
@@ -11,14 +11,15 @@ class PhotosModel extends Model
         $email = "tcosmin.pasat@gmail.com";
         $token = "d4q2gP4eRu8d72TC-l_Khb_tEZ6023JaRekNCOI2d8c";
 
+        echo "<div class=\"column\">" . PHP_EOL;
+
         for($index = 0; $index < 18; $index++) {
-            if ($index % 6 === 0)
+            if ($index % 6 === 0 && $index != 0) {
+                echo "</div>" . PHP_EOL;
                 echo "<div class=\"column\">" . PHP_EOL;
+            }
 
             echo "<img src=\"./img/art.jpg\">" . PHP_EOL;
-
-            if ($index % 5 === 0 && $index != 0)
-                echo "</div>" . PHP_EOL;
         }
     }
 
