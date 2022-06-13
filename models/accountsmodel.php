47dcd154aaebf58699cf49fa285e1d3207095150
DESKTOP-Q4F7MV9\Raluca <raluca1234rr@gmail.com>
2022-06-13 20:40:58 +0300

    .

--- models/accountsmodel.php
+++ models/accountsmodel.php
@@ -9,30 +9,9 @@ class AccountsModel extends Model
         $payload=json_decode(extractTokenPayload($token),true);
         $user_id=$payload['id'];
 
-        $this->setSql("select username from accounts");
-
-        $select_array = [
-            "user_id" => $user_id
-
-        ];
-
-        $sth = $this->conn->prepare($this->querry);
-
-        if ($sth->execute($select_array)) {
-            http_response_code(201);
-            echo json_encode(array("message" => "Account added."));
-            echo $this->getRow();
-            return true;
-        } else {
-            http_response_code(503);
-            echo json_encode(array("message" => "Unable to add account."));
-        }
-
-//        $result = pg_query($this->conn->, "SELECT author, email FROM authors");
-//        if (!$result) {
-//            echo "An error occurred.\n";
-//            exit;
-//        }
+        $this->setSql("select * from accounts");
+        $result = $this->getAll();
+        echo $result;
 
 //        while ($row = pg_fetch_row($result)) {
 //            echo "Author: $row[0]  E-mail: $row[1]";
