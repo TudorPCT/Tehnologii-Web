82db6521983d5919343ea4555d92f6bd46045484
DESKTOP-Q4F7MV9\Raluca <raluca1234rr@gmail.com>
2022-06-14 14:33:47 +0300

    .

--- views/templates/scripts/accountsscripts.js
+++ views/templates/scripts/accountsscripts.js
@@ -4,7 +4,7 @@ function getAccounts(){
 
     xmlhttp.onreadystatechange = function() {
         if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-            document.getElementById("accounts").innerHTML = this.responseText;
+            document.getElementById("accounts").value += this.responseText;
         }
 
     };
