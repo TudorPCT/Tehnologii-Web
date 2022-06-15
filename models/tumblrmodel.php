aedf876e9da04274548d4734c12ea0643dcf39e3
Hututuc Felix <felix.hututuc@gmail.com>
2022-06-15 17:19:51 +0300

    .

--- models/tumblrmodel.php
+++ models/tumblrmodel.php
@@ -62,6 +62,6 @@ class TumblrModel extends Model
         //echo $user;
 
         $respone = json_decode($user, true);
-        echo $response['response'];
+        echo $response['user'];
     }
 }
