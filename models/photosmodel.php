6dd74d47c13789bff35c712beb7e6fade759107b
TudorPCT <76231144+TudorPCT@users.noreply.github.com>
2022-06-16 14:30:07 +0300

    Update photosmodel.php

--- models/photosmodel.php
+++ models/photosmodel.php
@@ -41,7 +41,7 @@ class PhotosModel extends Model
                 echo  "<div class=\"column\">" . PHP_EOL;
             }
 
-            echo "<a href=\"socialmediabox.herokuapp.com/?load=photos/photo" . "\">" . PHP_EOL;
+            echo "<a href=\"./?load=photos/photo" . "\">" . PHP_EOL;
             echo "<img src=\"" . $unsplashPhotos[$index]["urls"]["full"] . "\">" . PHP_EOL;
             echo "</a>";
             $count++;
