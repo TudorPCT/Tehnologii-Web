b65a59e697198aa2a85b02a4db751cbaeaa9b0e4
Hututuc Felix <felix.hututuc@gmail.com>
2022-06-17 13:35:07 +0300

    .

--- controllers/photoscontroller.php
+++ controllers/photoscontroller.php
@@ -20,7 +20,7 @@ class PhotosController extends Controller
             } else if ($_GET['platform'] === 'tumblr' && isset($_GET['photo'])) {
                 $info = $this->model->getTumblrInfo($token, $_GET['id'], $_GET['photo']);
                 $data = ["link" => $info['url']];
-                print_r($data);
+                print_r($info);
             } else {
                 http_response_code(400);
                 die();
