e5f7c304916cc8b190f1a163f2a4d5017da02b67
Hututuc Felix <felix.hututuc@gmail.com>
2022-06-18 19:06:52 +0300

    .

--- controllers/tumblrcontroller.php
+++ controllers/tumblrcontroller.php
@@ -51,7 +51,7 @@ class TumblrController extends Controller
     }
 
     function postPhoto($token) {
-        echo "tumblrcontroller: " . $_POST['canvasImage'];
+        echo var_dump($_POST);
         //echo $this->model->postPhoto($token, $_FILES['canvasImage']);
     }
 
