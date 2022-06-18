2e3720f6c4f3d8632f962cd6933b97ae9326be41
Hututuc Felix <felix.hututuc@gmail.com>
2022-06-18 17:54:47 +0300

    .

--- views/templates/scripts/editPhoto.js
+++ views/templates/scripts/editPhoto.js
@@ -139,7 +139,7 @@ console.log("trimit poza prelucrata la server si el o posteaza pe contul meu tum
 
     xhr.onload = () => console.log(xhr.responseText);
 
-    var obj = {'url': photoUrl}
+    var obj = {'url': photoUrl};
     xhr.send(JSON.parse(obj));
 
 
