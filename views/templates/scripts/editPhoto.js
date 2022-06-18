c44061a5d6b852921e020323743af77e0d1b7f15
Hututuc Felix <felix.hututuc@gmail.com>
2022-06-19 00:58:09 +0300

    .

--- views/templates/scripts/editPhoto.js
+++ views/templates/scripts/editPhoto.js
@@ -96,7 +96,7 @@ function getImageEdited(){
         context.scale(scaleX,scaleY);
         // context.drawImage(image,0,0, canvas.width*scaleX,canvas.height*scaleY);
         context.drawImage(image,0,0,canvas.width,canvas.height);
-        return canvas.toDataURL();
+        return canvas.toDataURL("image/jpg");
     }
 }
 function Download_btn(){
@@ -105,7 +105,7 @@ function Download_btn(){
         const link = document.createElement("a");
             document.body.appendChild(link);
             link.setAttribute("href",jpgUrl);
-            link.setAttribute("download","photo.png");
+            link.setAttribute("download","photo.jpg");
             link.click();
             document.body.removeChild(link);
 }
