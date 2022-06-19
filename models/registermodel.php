0cf2a049d8bcd041761460f34ca404fb5a0a5724
TudorPCT <76231144+TudorPCT@users.noreply.github.com>
2022-06-19 11:06:55 +0300

    .

--- models/registermodel.php
+++ models/registermodel.php
@@ -16,23 +16,11 @@ class RegisterModel extends Model
     }
 
     function insert_user($data){
-        if(
-            !empty($data["first_name"]) &&
-            !empty($data["last_name"]) &&
-            !empty($data["email"]) &&
-            !empty($data["password"])
-        ){
-
-            if(0 != strcmp($data["password"], $data["password2"])){
+            if(0 != strcmp($data["password"], $data["password2"])
+                    || !$this->check_user($data["email"])
+                    || !filter_var($data["email"], FILTER_VALIDATE_EMAIL)){
                 http_response_code(400);
-                echo json_encode(array("message" => "Unable to create user. Passwords don't match."));
-            }else if(!$this->check_user($data["email"])){
-                http_response_code(409);
-                echo json_encode(array("message" => "Unable to create user. Already exists"));
-            }else if(!filter_var($data["email"], FILTER_VALIDATE_EMAIL))
-            {
-                http_response_code(409);
-                echo json_encode(array("message" => "Email format not valid"));
+                echo json_encode(array("message" => "Unable to create user"));
             }
             else
             {
@@ -48,19 +36,9 @@ class RegisterModel extends Model
                 $sth = $this->conn->prepare($this->querry);
 
                 if ($sth->execute($insert_array)) {
-                    http_response_code(201);
-                    echo json_encode(array("message" => "User added."));
                     return true;
-                } else {
-                    http_response_code(503);
-                    echo json_encode(array("message" => "Unable to add user."));
                 }
             }
-        }
-        else{
-            http_response_code(400);
-            echo json_encode(array("message" => "Unable to create user. Need more data."));
-        }
         return false;
     }
 }
