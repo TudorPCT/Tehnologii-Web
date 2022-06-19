e7c34685d4c101c76a63efdfbf07d5b0c88a4417
TudorPCT <76231144+TudorPCT@users.noreply.github.com>
2022-06-20 00:27:30 +0300

    .

--- views/templates/scripts/fileUpload.js
+++ views/templates/scripts/fileUpload.js
@@ -1,15 +1,6 @@
 
 var output = document.querySelector('#images_preview');
 output.innerHTML = '';
-var image = new Image();
-image.id = Math.random().toString(36).substr(2, 9);
-image.height = 80;
-image.width = 80;
-image.src = "https://images.unsplash.com/photo-1493612276216-ee3925520721?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tfGVufDB8fDB8fA%3D%3D&w=1000&q=80";
-output.appendChild(image);
-image.ondragstart = function (e) {
-    e.dataTransfer.setData("text", e.target.id);
-};
 
 loadUnsplashPhotos();
 function loadUnsplashPhotos() {
