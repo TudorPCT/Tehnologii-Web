5963486043e5eeba49d0f52a440c68b2277b32e8
TudorPCT <76231144+TudorPCT@users.noreply.github.com>
2022-06-20 00:19:07 +0300

    .

--- views/templates/scripts/fileUpload.js
+++ views/templates/scripts/fileUpload.js
@@ -1,28 +1,39 @@
 
 var output = document.querySelector('#images_preview');
 output.innerHTML = '';
+var image = new Image();
+image.id = Math.random().toString(36).substr(2, 9);
+image.height = 80;
+image.width = 80;
+image.src = "https://images.unsplash.com/photo-1493612276216-ee3925520721?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tfGVufDB8fDB8fA%3D%3D&w=1000&q=80";
+output.appendChild(image);
+image.ondragstart = function (e) {
+    e.dataTransfer.setData("text", e.target.id);
+};
 
-
+loadUnsplashPhotos();
 function loadUnsplashPhotos() {
     var xmlhttp = new XMLHttpRequest();
-    $link = "./?load=photos/getCollagePhotos";
-    $link = $link.concat("&minLikes=", $filters[0],
+    link = "socialmediabox.herokuapp.com/?load=photos/getCollagePhotos";
+
+    link = link.concat("&minLikes=", 0,
         "&maxLikes=", 0,
         "&minShares=", 0,
         "&maxShares=", 0,
         "&postDate=", 0);
-    console.log($link);
-    xmlhttp.open("GET", $link, true);
-
-    xmlhttp.send();
-
 
     xmlhttp.onreadystatechange = function () {
         if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-            let photos = JSON.parse(this.responseText);
+            photos = JSON.parse(this.responseText);
+            console.log(photos);
             photos.forEach(printPhoto);
-        }
+        }else console.log(this.status);
     }
+
+    xmlhttp.open("GET", link, true);
+
+    xmlhttp.send();
+
 }
 
 function printfPhoto(photo){
